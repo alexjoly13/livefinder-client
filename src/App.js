3126f7dbe79bedfb52c770ea47a6b771a5be258d
Jules Faivre <faivre.jules@gmail.com>
2019-02-28 14:37:10 +0100

    Spotify connect step 2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,45 @@ import NotFound from "./components/NotFound.js";
 import Connected from "./components/connected";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    let userData = localStorage.getItem("currentUser");
+    if (userData) {
+      userData = JSON.parse(userData);
+    }
+
+    this.state = {
+      currentUser: userData
+    };
+  }
+
+  updateUser(userInfo) {
+    if (userInfo) {
+      localStorage.setItem("currentUser", JSON.stringify(userInfo));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+    this.setState({ currentUser: userInfo });
+  }
+
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/connected/:loginToken?" component={Connected} />
+          <Route
+            path="/connected/:loginToken?"
+            render={props => {
+              return (
+                <Connected
+                  currentUser={this.state.currentUser}
+                  loggedIn={user => this.updateUser(user)}
+                  match={props.match}
+                  history={props.history}
+                />
+              );
+            }}
+          />{" "}
           <Route component={NotFound} />
         </Switch>
       </div>
--- src/components/connected.js
+++ src/components/connected.js
@@ -4,9 +4,7 @@ import axios from "axios";
 class Connected extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentUser: null
-    };
+    this.state = {};
   }
 
   componentDidMount() {
@@ -17,8 +15,8 @@ class Connected extends Component {
         .post("http://localhost:8888/auth/token-login", match.params)
         .then(response => {
           console.log("Logged-In", response.data);
-          this.setState({ currentUser: response.data });
           history.replace("/connected");
+          this.props.loggedIn(response.data);
         })
         .catch(err => alert("poop"));
     }
